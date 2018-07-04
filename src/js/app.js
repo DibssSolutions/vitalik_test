567167210eeed11137c9fa254374a4bf38df7902
KopylovVitalik <vitalik.kopylov@gmail.com>
2018-07-04 18:00:52 +0300

    almost all

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,7 @@
 import sayHello from './lib/sayHello.js';
 
 sayHello();
+
+document.querySelector('.lang-changer').addEventListener('mouseover', (e) => {
+  document.querySelector('.lang-changer').classList.toggle('lang-changer--open');
+});
