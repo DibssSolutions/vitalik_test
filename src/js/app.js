2795c0b1f52c306259da572d31a78f483699e0d4
KopylovVitalik <vitalik.kopylov@gmail.com>
2018-07-09 18:31:16 +0300

    added tokens

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,18 @@ import sayHello from './lib/sayHello.js';
 
 sayHello();
 
-document.querySelector('.lang-changer').addEventListener('mouseover', (e) => {
-  document.querySelector('.lang-changer').classList.toggle('lang-changer--open');
+// document.querySelector('.lang-changer').addEventListener('mouseover', (e) => {
+//   document.querySelector('.lang-changer').classList.toggle('lang-changer--open');
+// });
+
+const video = document.getElementById('video');
+let playing = false;
+
+document.querySelector('.tv__play').addEventListener('click', e => {
+  if (playing) {
+    video.pause();
+  } else {
+    video.play();
+  }
+  playing = !playing;
 });
